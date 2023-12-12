080e31f40d1e82ab36dff988aad01b817c7d363b
21bd1a05k6csed@gmail.com <21bd1a05k6csed@gmail.com>
2023-12-12 11:28:36 +0530

    demo for team

---
+++ src/main/java/in/kmit/webdemo/App.java
@@ -0,0 +1,13 @@
+package in.kmit.webdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
